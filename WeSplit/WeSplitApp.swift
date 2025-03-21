96aec04bd4fa1e29935f3a051a07e9d7defe775a
Lukas Rucky <ruckylukas@seznam.cz>
2025-03-21 15:03:23 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Lukáš Rucký on 21.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
